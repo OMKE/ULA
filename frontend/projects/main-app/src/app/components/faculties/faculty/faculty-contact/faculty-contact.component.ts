86b960beaa48d89c1c42bb614f232274bbdf8d93
Omar Iriskic <noisewavehd@gmail.com>
2020-06-01 05:58:11 +0200

    Added Contact Faculty component

---
+++ frontend/projects/main-app/src/app/components/faculties/faculty/faculty-contact/faculty-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-contact',
+  templateUrl: './faculty-contact.component.html',
+  styleUrls: ['./faculty-contact.component.scss']
+})
+export class FacultyContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
