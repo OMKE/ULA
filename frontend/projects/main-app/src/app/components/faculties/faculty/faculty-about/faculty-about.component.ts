fef6373cbf48d9cf37ca6d743958f124c131eb01
Omar Iriskic <noisewavehd@gmail.com>
2020-06-01 05:57:54 +0200

    Added About Faculty component

---
+++ frontend/projects/main-app/src/app/components/faculties/faculty/faculty-about/faculty-about.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-about',
+  templateUrl: './faculty-about.component.html',
+  styleUrls: ['./faculty-about.component.scss']
+})
+export class FacultyAboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
