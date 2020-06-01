091df0899f6720bb608a59a0cc95f0ccfe31952b
Omar Iriskic <noisewavehd@gmail.com>
2020-06-01 05:59:46 +0200

    Added Faculty Location component

---
+++ frontend/projects/main-app/src/app/components/faculties/faculty/faculty-location/faculty-location.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faculty-location',
+  templateUrl: './faculty-location.component.html',
+  styleUrls: ['./faculty-location.component.scss']
+})
+export class FacultyLocationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
