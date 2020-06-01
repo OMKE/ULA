095a20485994350732a7e00aa9fd87faed2ec3e2
Omar Iriskic <noisewavehd@gmail.com>
2020-06-01 05:54:01 +0200

    Added animations to play button component

--- frontend/projects/main-app/src/app/shared/layout/play-button/play-button.component.ts
+++ frontend/projects/main-app/src/app/shared/layout/play-button/play-button.component.ts
@@ -12,11 +12,14 @@ export class PlayButtonComponent implements OnInit {
 
   @Output() onPlay: EventEmitter<Video> = new EventEmitter();
 
+  active: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
 
   play() {
+    this.active = true;
     // We just want to catch this event, we will already know on parent component which video we want to play
     this.onPlay.emit({ id: null, name: "", format: "" });
   }
