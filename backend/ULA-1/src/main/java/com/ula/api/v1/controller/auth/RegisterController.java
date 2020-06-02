b957b65db10e6a51e58939c86efd0d1b17991e99
Omar Iriskic <noisewavehd@gmail.com>
2020-06-02 04:30:05 +0200

    Refactored register method

--- backend/ULA-1/src/main/java/com/ula/api/v1/controller/auth/RegisterController.java
+++ backend/ULA-1/src/main/java/com/ula/api/v1/controller/auth/RegisterController.java
@@ -11,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ula.api.v1.request.auth.MethodArgumentExceptionResolver;
+import com.ula.api.v1.request.MethodArgumentExceptionResolver;
 import com.ula.api.v1.request.auth.RegisterRequest;
 import com.ula.dto.model.UserDTO;
 import com.ula.dto.response.Response;
 import com.ula.service.exception.PasswordsDontMatchException;
+import com.ula.service.exception.RequiredFieldException;
 import com.ula.service.exception.UserException;
 import com.ula.service.user.UserServiceImpl;
 
@@ -36,20 +37,25 @@ public class RegisterController {
 	public Response<Object> register(
 			@RequestBody @Valid RegisterRequest registerRequest,
 			Errors errors)
-			throws UserException
 	{
-
-		UserDTO userDTO = new UserDTO().setUsername(registerRequest.getUsername())
-				.setEmail(registerRequest.getEmail())
-				.setPassword(passwordEncoder.encode(registerRequest
-						.getPassword()))
-				.setFirstName(
-						registerRequest.getFirstName())
-				.setLastName(registerRequest.getLastName());
-
 		try {
+			MethodArgumentExceptionResolver.checkFields(registerRequest,
+					registerRequest.getUsername(), registerRequest.getPassword(),
+					registerRequest.getEmail(), registerRequest.getFirstName(),
+					registerRequest.getLastName(), registerRequest.getAcceptTerms());
+
+			UserDTO userDTO = new UserDTO().setUsername(registerRequest.getUsername())
+					.setEmail(registerRequest.getEmail())
+					.setPassword(passwordEncoder.encode(registerRequest.getPassword()))
+					.setFirstName(registerRequest.getFirstName())
+					.setLastName(registerRequest.getLastName())
+					.setTermsAndConditions(registerRequest.getAcceptTerms());
+
 //			Check if password and confirmation password match
-			this.checkForPasswords(registerRequest);
+userService.checkForPasswords(registerRequest.getPassword(), registerRequest.getConfirmPassword());
+userService.checkTermsAndConditions(registerRequest.getAcceptTerms());
+
+//			If everything is OK, return OK response
 			return Response.ok().setPayload(userService.add(userDTO));
 		} catch (UserException e) {
 			return Response.badRequest()
@@ -58,17 +64,14 @@ public class RegisterController {
 		{
 			return Response.validationException()
 					.setErrors(MethodArgumentExceptionResolver.resolve(errors, e));
+		} catch (RequiredFieldException e)
+		{
+			return Response.badRequest()
+					.setErrors(MethodArgumentExceptionResolver.resolve(errors));
 		}
 
 		
 	}
 
-	public void checkForPasswords(RegisterRequest registerRequest)
-			throws PasswordsDontMatchException
-	{
-		if (!registerRequest.getPassword().equals(registerRequest.getConfirmPassword()))
-		{
-			throw new PasswordsDontMatchException("Passwords don't match");
-		}
-	}
+
 }
