9c173559b1a59b4d0812eea7814c734abca83694
Omar Iriskic <noisewavehd@gmail.com>
2020-08-11 01:32:58 +0200

    Added custom response class

---
+++ backend/ULA-UNIVERSITY-SERVICE/src/main/java/com/ula/university/dto/response/Response.java
@@ -0,0 +1,84 @@
+package com.ula.university.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Response<T> {
+
+
+
+    private Status status;
+    private T payload;
+    private Object errors;
+    private Object metadata;
+
+    public static <T> Response<T> badRequest() {
+
+        Response<T> response = new Response<>();
+        response.setStatus(Status.BAD_REQUEST);
+        return response;
+    }
+
+    public static <T> Response<T> ok() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.OK);
+        return response;
+    }
+
+    public static <T> Response<T> unauthorized() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.UNAUTHORIZED);
+        return response;
+    }
+
+    public static <T> Response<T> validationException() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.VALIDATION_EXCEPTION);
+        return response;
+    }
+
+    public static <T> Response<T> wrongCredentials() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.WRONG_CREDENTIALS);
+        return response;
+    }
+
+    public static <T> Response<T> accessDenied() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.ACCESS_DENIED);
+        return response;
+    }
+
+    public static <T> Response<T> methodNotAllowed() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.METHOD_NOT_ALLOWED);
+        return response;
+    }
+
+    public static <T> Response<T> exception() {
+        Response<T> response = new Response<>();
+        response.setStatus(Status.EXCEPTION);
+        return response;
+    }
+
+    public static ResponseError addErrorMessageToResponse(Response<Object> response,
+                                                          String message) {
+        ResponseError error = new ResponseError();
+        error.setText(message);
+        response.setErrors(error);
+        return error;
+    }
+
+    public enum Status {
+        OK, BAD_REQUEST, UNAUTHORIZED, VALIDATION_EXCEPTION, EXCEPTION, WRONG_CREDENTIALS,
+        ACCESS_DENIED, METHOD_NOT_ALLOWED
+    }
+}
