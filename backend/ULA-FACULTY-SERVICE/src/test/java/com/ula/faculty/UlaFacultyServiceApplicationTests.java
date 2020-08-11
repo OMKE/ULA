c2350233c055ceca49b6a7887cdfef076289356c
Omar Iriskic <noisewavehd@gmail.com>
2020-08-11 06:31:09 +0200

    Added faculty service

---
+++ backend/ULA-FACULTY-SERVICE/src/test/java/com/ula/faculty/UlaFacultyServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ula.faculty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlaFacultyServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
