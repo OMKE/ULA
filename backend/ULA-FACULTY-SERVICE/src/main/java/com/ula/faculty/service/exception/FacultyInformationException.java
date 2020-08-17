440ffb6dcc8442e04b2582f7227a606fa34d2b7c
Omar Iriskic <noisewavehd@gmail.com>
2020-08-17 20:34:48 +0200

    Added exceptions

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/service/exception/FacultyInformationException.java
@@ -0,0 +1,15 @@
+package com.ula.faculty.service.exception;
+
+
+public class FacultyInformationException extends Exception {
+
+    public FacultyInformationException(String message) 
+    {
+        super(message);
+    }
+    public FacultyInformationException(Throwable throwable)
+    {
+        super(throwable);
+    }
+
+}
---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/service/exception/StudyProgramDegreeException.java
@@ -0,0 +1,15 @@
+package com.ula.faculty.service.exception;
+
+
+public class StudyProgramDegreeException extends Exception {
+
+    public StudyProgramDegreeException(String message) 
+    {
+        super(message);
+    }
+    public StudyProgramDegreeException(Throwable throwable)
+    {
+        super(throwable);
+    }
+
+}
