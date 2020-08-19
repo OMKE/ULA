65993cc231ca62b02fafd54377fbdd9097a980d3
Omar Iriskic <noisewavehd@gmail.com>
2020-08-19 08:39:43 +0200

    Added DTO class for StudyProgram when there is paging and sorting

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/dto/model/StudyProgramDTOWithNumberOfPages.java
@@ -0,0 +1,17 @@
+package com.ula.faculty.dto.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class StudyProgramDTOWithNumberOfPages
+{
+    List<StudyProgramDTO> studyPrograms;
+    int totalPages;
+
+}
