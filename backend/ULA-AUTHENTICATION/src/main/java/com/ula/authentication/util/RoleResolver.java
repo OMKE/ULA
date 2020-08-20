7ee0cad3039cb615d5882476cb6bb745656897b7
Omar Iriskic <noisewavehd@gmail.com>
2020-08-20 12:43:43 +0200

    Added RoleResolver to get roles from resource server annotations

---
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/util/RoleResolver.java
@@ -0,0 +1,49 @@
+package com.ula.authentication.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RoleResolver
+{
+    // Gets roles in form:
+    // case-1: USER
+    // case-2: [USER,ADMIN]
+    // case-3: USER,ADMIN
+    private String value;
+
+    private ArrayList<String> roles = new ArrayList<>();
+
+    public RoleResolver(String value)
+    {
+        this.value = value;
+        this.resolve();
+    }
+
+    private void resolve()
+    {
+        // Check if it's list (separated by comma, or it's closed with brackets), else if there is just one role add as one role
+        if(this.value.contains("[") && this.value.contains(",") && this.value.contains("]")) {
+            this.value = this.value.replace("[", "").replace("]", "");
+
+            this.roles.addAll(Arrays.asList(this.value.split(",")));
+        } else if (!this.value.contains("[") && this.value.contains("]"))
+        {
+            throw new IllegalArgumentException("@Authorized annotation is missing opening bracket");
+        } else if (this.value.contains("[") && !this.value.contains("]"))
+        {
+            throw new IllegalArgumentException("@Authorized annotation is missing closing bracket");
+        } else if (this.value.contains(",") && !this.value.contains("[") && !this.value.contains("]"))
+        {
+            // if for some reason we only separated roles with commas (without brackets) throwing exception would be unnecessary
+            this.roles.addAll(Arrays.asList(this.value.split(",")));
+        }
+        else {
+            this.roles.add(this.value);
+        }
+    }
+
+
+    public ArrayList<String> getRoles() { return this.roles; }
+
+
+}
