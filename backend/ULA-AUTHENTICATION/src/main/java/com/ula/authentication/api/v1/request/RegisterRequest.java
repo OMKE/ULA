7716724feb9568fb5d46ae79825e2bdd4c98899b
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-25 04:06:59 +0200

    Changed to min and max size so they have different message

--- backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/api/v1/request/RegisterRequest.java
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/api/v1/request/RegisterRequest.java
@@ -18,26 +18,32 @@ import javax.validation.constraints.Size;
 public class RegisterRequest {
 
 	@NotEmpty(message = "Username is required")
-	@Size(min = 4, max = 32, message = "Username must be at least 5 characters long")
+	@Size(min = 5, message = "Username must be at least 5 characters long")
+	@Size(max = 32, message = "Username can not be longer than 32 characters")
 	private String username;
 
 	@NotEmpty(message = "Password is required")
-	@Size(min = 6, max = 32, message = "Password must be at least 5 characters long")
+	@Size(min = 6, message = "Password must be at least 5 characters long")
+	@Size(max = 32, message = "Password can not be longer than 32 characters")
 	private String password;
 
 	@NotEmpty(message = "Password confirmation is required")
-	@Size(min = 6, max = 32, message = "Confirmation password must be at least 5 characters long")
+	@Size(min = 6, message = "Confirmation password must be at least 6 characters long")
+	@Size(max = 32, message = "Confirmation password can not be longer than 32 characters")
 	private String confirmPassword;
 
 	@NotEmpty(message = "Email is required")
 	@Email(message = "Email must be valid")
 	private String email;
 
-	@Size(min = 3, max = 64, message = "First name must be at least 3 characters long")
+
+	@Size(min = 3, message = "First name must be at least 3 characters long")
+	@Size(max = 64, message = "First name can not be longer than 64 characters")
 	@NotEmpty(message = "First name is required")
 	private String firstName;
 
-	@Size(min = 3, max = 64, message = "Last name must be at least 3 characters long")
+	@Size(min = 3, message = "Last name must be at least 3 characters long")
+	@Size(max = 64, message = "Last name can not be longer than 64 characters")
 	@NotEmpty(message = "Last name is required")
 	private String lastName;
 
