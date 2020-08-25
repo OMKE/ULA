1a45eb45755b58d2f92c4ac243bebe2ced43c76c
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-25 04:09:40 +0200

    Added ComponentScan so we can inject classes from ULA-CORE

--- backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/UlaAuthenticationApplication.java
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/UlaAuthenticationApplication.java
@@ -3,9 +3,11 @@ package com.ula.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableEurekaClient
 @SpringBootApplication
+@ComponentScan({"com.ula.authentication","org.ula.core"})
 public class UlaAuthenticationApplication
 {
 
