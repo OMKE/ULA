1ff385ab9537ae7a8b3e276f71f79937e06e4700
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-25 02:49:54 +0200

    Added ComponentScan and FeignClients base packages so we can use them from ULA-CORE

--- backend/ULA-UNIVERSITY-SERVICE/src/main/java/com/ula/university/UlaUniversityServiceApplication.java
+++ backend/ULA-UNIVERSITY-SERVICE/src/main/java/com/ula/university/UlaUniversityServiceApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 
 @EnableEurekaClient
 @EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.COLLECTION_JSON)
-@EnableFeignClients
+@EnableFeignClients(basePackages = {"org.ula.core.feign", "com.ula.university.feign"})
 @EnableCircuitBreaker
+@ComponentScan({"com.ula.university","org.ula.core"})
 @SpringBootApplication
 public class UlaUniversityServiceApplication
 {
