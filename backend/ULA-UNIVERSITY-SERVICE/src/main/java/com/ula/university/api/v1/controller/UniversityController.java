94f34a6168deff356e1ff169443ecb98f0abf18f
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-25 02:49:18 +0200

    Changed to Response imported from org.ula.core.api.response

--- backend/ULA-UNIVERSITY-SERVICE/src/main/java/com/ula/university/api/v1/controller/UniversityController.java
+++ backend/ULA-UNIVERSITY-SERVICE/src/main/java/com/ula/university/api/v1/controller/UniversityController.java
@@ -1,14 +1,13 @@
 package com.ula.university.api.v1.controller;
 
-import com.ula.university.dto.response.Response;
+
 import com.ula.university.service.exception.UniversityInformationNotFoundException;
 import com.ula.university.service.exception.UniversityNotFoundException;
 import com.ula.university.service.university.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.ula.core.api.response.Response;
 
 
 @RestController
