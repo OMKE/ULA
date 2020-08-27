df79ed5997d77b47650599ef07b2a99558847562
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-27 09:02:31 +0200

    Added service for serving static content

---
+++ backend/ULA-STATIC-CONTENT-SERVICE/src/test/java/com/ula/content/UlaStaticContentServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ula.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlaStaticContentServiceApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
