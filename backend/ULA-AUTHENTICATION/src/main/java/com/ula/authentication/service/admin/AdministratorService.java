34edfde4310e97d75862fd21b192dc5476f9a31e
Lazar Filipovic <laki098@gmail.com>
2020-08-28 01:21:50 +0200

    Added AdminRepository and Service

---
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/service/admin/AdministratorService.java
@@ -0,0 +1,19 @@
+package com.ula.authentication.service.admin;
+
+import com.ula.authentication.dto.model.AdminDTO;
+import com.ula.authentication.service.exception.AdministratorNotFoundException;
+import com.ula.authentication.service.exception.UserIsAlreadyAdministratorException;
+import com.ula.authentication.service.exception.UserNotFoundException;
+import com.ula.authentication.service.exception.UserPermissionException;
+
+import java.util.List;
+
+public interface AdministratorService
+{
+
+    List<AdminDTO> index();
+    AdminDTO show(Long id) throws AdministratorNotFoundException;
+    String store(Long userId) throws UserNotFoundException, UserIsAlreadyAdministratorException;
+    String delete(Long userId) throws UserNotFoundException, UserPermissionException, AdministratorNotFoundException;
+
+}
