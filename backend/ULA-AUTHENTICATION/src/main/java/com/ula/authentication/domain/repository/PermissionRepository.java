76a664cd58e2a82a0df52cd927d9bec0dc7d2869
Lazar Filipovic <laki098@gmail.com>
2020-08-28 01:21:20 +0200

    Added ROLE_ADMIN query

--- backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/domain/repository/PermissionRepository.java
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/domain/repository/PermissionRepository.java
@@ -11,17 +11,20 @@ public interface PermissionRepository extends CrudRepository<Permission, Long>
 {
 
 	@Query("select e from #{#entityName} e where e.title='USER'")
-	public Permission ROLE_USER();
+	Permission ROLE_USER();
 
 	@Query("select e from #{#entityName} e where e.title='STAFF'")
-	public Permission ROLE_STAFF();
+	Permission ROLE_STAFF();
 
 	@Query("select e from #{#entityName} e where e.title='CONTENT_CREATOR'")
-	public Permission ROLE_CONTENT_CREATOR();
+	Permission ROLE_CONTENT_CREATOR();
 
 	@Query("select e from #{#entityName} e where e.title='TEACHER'")
-	public Permission ROLE_TEACHER();
+	Permission ROLE_TEACHER();
 
 	@Query("select e from #{#entityName} e where e.title='STUDENT'")
-	public Permission ROLE_STUDENT();
+	Permission ROLE_STUDENT();
+
+	@Query("select e from #{#entityName} e where e.title='ADMIN'")
+	Permission ROLE_ADMIN();
 }
