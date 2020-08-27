67b4d0f0717d57e4e766e01f21fb9ebafaf35086
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-27 09:28:05 +0200

    Added hystrix fallback for static content  feign client

--- backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/UlaAuthenticationApplication.java
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/UlaAuthenticationApplication.java
@@ -2,6 +2,7 @@ package com.ula.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
+@EnableCircuitBreaker
 @ComponentScan({"com.ula.authentication","org.ula.core"})
 public class UlaAuthenticationApplication
 {
