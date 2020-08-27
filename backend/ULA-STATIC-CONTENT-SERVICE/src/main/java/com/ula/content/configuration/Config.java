f62b06d527db8c2a91bf47a2f30736dcf5a9ef49
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-27 09:06:55 +0200

    Added message converters sp we can work with MultiPart file

--- backend/ULA-STATIC-CONTENT-SERVICE/src/main/java/com/ula/content/configuration/Config.java
+++ backend/ULA-STATIC-CONTENT-SERVICE/src/main/java/com/ula/content/configuration/Config.java
@@ -29,4 +29,43 @@ public class Config implements WebMvcConfigurer
     {
         resolvers.add(new TokenResolver());
     }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters)
+    {
+        converters.add(mappingJackson2HttpMessageConverter());
+        converters.add(byteArrayHttpMessageConverter());
+    }
+
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter()
+    {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        return converter;
+    }
+
+    @Bean
+    public ByteArrayHttpMessageConverter byteArrayHttpMessageConverter()
+    {
+        ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+        arrayHttpMessageConverter.setSupportedMediaTypes(getSupportedMediaTypes());
+        return arrayHttpMessageConverter;
+    }
+    private List<MediaType> getSupportedMediaTypes() {
+        List<MediaType> list = new ArrayList<MediaType>();
+        list.add(MediaType.IMAGE_JPEG);
+        list.add(MediaType.IMAGE_PNG);
+        list.add(MediaType.APPLICATION_OCTET_STREAM);
+        return list;
+    }
 }
