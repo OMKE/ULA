a4e050a2477a2acfc02d4f4bccb20ea1a1aa2104
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-27 09:07:38 +0200

    Added DTO classes for future use

---
+++ backend/ULA-STATIC-CONTENT-SERVICE/src/main/java/com/ula/content/dto/FileDTO.java
@@ -0,0 +1,15 @@
+package com.ula.content.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+public class FileDTO
+{
+    private String name;
+}
