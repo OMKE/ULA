7000f45d89ed6a2017dd7a253531f1cdfe2e5ad3
Omar Iriskic <omaririskic.dev@gmail.com>
2020-08-27 09:09:55 +0200

    Added UserDTO to core so we can use it accross the services

---
+++ backend/ULA-CORE/src/main/java/org/ula/core/domain/model/UserDTO.java
@@ -0,0 +1,24 @@
+package org.ula.core.domain.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+public class UserDTO
+{
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImage;
+    private String firstName;
+    private String lastName;
+    private String termsAndConditions;
+
+
+}
