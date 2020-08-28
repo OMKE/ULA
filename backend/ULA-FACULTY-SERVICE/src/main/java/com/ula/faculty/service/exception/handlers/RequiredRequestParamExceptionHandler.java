87f1c6bb94ac504ae6ffbd6e690b7d8239f31fcc
Lazar Filipovic <laki098@gmail.com>
2020-08-28 10:01:54 +0200

    Added global exception handlers

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/service/exception/handlers/RequiredRequestParamExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ula.faculty.service.exception.handlers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.ula.core.exception.handler.RequiredRequestParameterExceptionHandler;
+
+@RestControllerAdvice
+public class RequiredRequestParamExceptionHandler extends RequiredRequestParameterExceptionHandler
+{
+}
