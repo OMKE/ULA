85648e6b26b596fae0d1b711a098930df77eaa48
Lazar Filipovic <laki098@gmail.com>
2020-08-29 08:15:45 +0200

    Added faculty repo and changed faculty icon nullable

--- backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/domain/repository/FacultyRepository.java
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/domain/repository/FacultyRepository.java
@@ -2,6 +2,7 @@ package com.ula.faculty.domain.repository;
 
 import com.ula.faculty.domain.model.Faculty;
 import org.springframework.stereotype.Repository;
+import org.ula.core.domain.repository.CrudRepository;
 
 @Repository
 public interface FacultyRepository extends CrudRepository<Faculty, Long>
