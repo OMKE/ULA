efa38fc3f1f803c3d5e9c04bb4dd056de932627a
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-02 18:41:34 +0200

    Added StudentOnYear service

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/service/studentonyyear/StudentOnYearService.java
@@ -0,0 +1,30 @@
+package com.ula.faculty.service.studentonyyear;
+
+import com.ula.faculty.api.v1.request.StoreStudentOnYearRequest;
+import com.ula.faculty.api.v1.request.UpdateStudentOnYearRequest;
+import com.ula.faculty.domain.model.Subject;
+import com.ula.faculty.dto.model.StudentOnYearDTO;
+import com.ula.faculty.service.exception.*;
+
+import java.util.List;
+import java.util.Set;
+
+public interface StudentOnYearService
+{
+    List<StudentOnYearDTO> index();
+
+    StudentOnYearDTO show(Long id)
+    throws StudentOnYearNotFoundException;
+
+    String store(String token, StoreStudentOnYearRequest studentOnYearDTO)
+    throws StudentOnYearNotFoundException, StudentNotFoundException, YearOfStudyNotFoundException, StudentOnYearConflictException, SubjectRealizationNotFoundException;
+
+    void storeSubjectAttendanceBasedOnSubjects(Set<Subject> subjects, Long studentId)
+    throws SubjectRealizationNotFoundException, StudentNotFoundException, StudentOnYearNotFoundException;
+
+    String addYearOfStudy(Long id, UpdateStudentOnYearRequest studentOnYearDTO, String token)
+    throws StudentNotFoundException, YearOfStudyNotFoundException, StudentOnYearConflictException, StudentOnYearNotFoundException, SubjectRealizationNotFoundException;
+
+    String delete(Long id)
+    throws StudentOnYearNotFoundException;
+}
