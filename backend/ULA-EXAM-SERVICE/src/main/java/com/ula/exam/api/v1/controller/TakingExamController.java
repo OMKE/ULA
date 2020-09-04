960549f8e7e5f9c33aa7ab94fb120f60e1c6c9c6
Lazar Filipovic <laki098@gmail.com>
2020-09-04 20:38:37 +0200

    Added TakingExamController

---
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/api/v1/controller/TakingExamController.java
@@ -0,0 +1,114 @@
+package com.ula.exam.api.v1.controller;
+
+import com.ula.exam.api.v1.request.StoreTakingExamRequest;
+import com.ula.exam.api.v1.request.UpdateTakingExamRequest;
+import com.ula.exam.dto.model.TakingExamDTO;
+import com.ula.exam.service.exception.SubjectAttendanceConflictException;
+import com.ula.exam.service.exception.SubjectAttendanceNotFoundException;
+import com.ula.exam.service.exception.TakingExamNotFoundException;
+import com.ula.exam.service.takingexam.TakingExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.ula.core.annotation.Authorized;
+import org.ula.core.annotation.Token;
+import org.ula.core.api.BaseController;
+import org.ula.core.api.response.Response;
+import org.ula.core.util.JWT;
+
+import javax.validation.Valid;
+
+@RestController
+@Validated
+public class TakingExamController extends BaseController
+{
+    @Autowired
+    private TakingExamService takingExamService;
+
+    @Authorized("[ADMIN,TEACHER]")
+    @GetMapping("/taking-exam")
+    public Response<Object> index
+    (
+            @Token JWT jwt
+    )
+    {
+        return Response.ok()
+                       .setPayload(this.takingExamService.index());
+    }
+
+    @Authorized("[ADMIN,TEACHER]")
+    @GetMapping("/taking-exam/{id}")
+    public Response<Object> show
+    (
+            @Token JWT jwt,
+            @PathVariable("id") Long id
+    )
+    {
+        try {
+            return Response.ok()
+                           .setPayload(this.takingExamService.show(id));
+        } catch (TakingExamNotFoundException e) {
+            return Response.exception().setErrors(e.getMessage());
+        }
+    }
+
+    @Authorized("[ADMIN,TEACHER]")
+    @PostMapping("/taking-exam")
+    public Response<Object> store
+    (
+            @Token JWT jwt,
+            @Valid @RequestBody StoreTakingExamRequest storeRequest,
+            Errors errors
+    )
+    {
+        TakingExamDTO takingExamDTO = new TakingExamDTO()
+                    .setNote(this.sanitize(storeRequest.getNote()))
+                    .setSubjectAttendanceId(storeRequest.getSubjectAttendanceId());
+
+        try {
+            return Response.ok()
+                           .setPayload(this.takingExamService.store(takingExamDTO, jwt.getContent()));
+        } catch (SubjectAttendanceNotFoundException | SubjectAttendanceConflictException e) {
+            return Response.exception().setErrors(e.getMessage());
+        }
+    }
+
+    @Authorized("[ADMIN,TEACHER]")
+    @PutMapping("/taking-exam/{id}")
+    public Response<Object> update
+    (
+            @Token JWT jwt,
+            @Valid @RequestBody UpdateTakingExamRequest updateRequest,
+            @PathVariable("id") Long id,
+            Errors errors
+    )
+    {
+        TakingExamDTO takingExamDTO = new TakingExamDTO()
+                    .setNote(this.sanitize(updateRequest.getNote()))
+                    .setPoints(updateRequest.getPoints());
+
+        try {
+            return Response.ok()
+                           .setPayload(this.takingExamService.update(id, takingExamDTO));
+        } catch (TakingExamNotFoundException e) {
+            return Response.exception().setErrors(e.getMessage());
+        }
+    }
+
+    @Authorized("ADMIN")
+    @DeleteMapping("/taking-exam/{id}")
+    public Response<Object> delete
+    (
+            @Token JWT jwt,
+            @PathVariable("id") Long id
+    )
+    {
+        try {
+            return Response.ok()
+                           .setPayload(this.takingExamService.delete(id));
+        } catch (TakingExamNotFoundException e) {
+            return Response.exception().setErrors(e.getMessage());
+        }
+    }
+}
