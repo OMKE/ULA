ddcf2de5d2d898d3ac6de36e681842753e1473a0
Lazar Filipovic <laki098@gmail.com>
2020-09-04 20:38:00 +0200

    Added Repository Clasese

---
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/domain/repository/TakingExamRepository.java
@@ -0,0 +1,13 @@
+package com.ula.exam.domain.repository;
+
+import com.ula.exam.domain.model.TakingExam;
+import org.springframework.stereotype.Repository;
+import org.ula.core.domain.repository.CrudRepository;
+
+import java.util.Optional;
+
+@Repository
+public interface TakingExamRepository extends CrudRepository<TakingExam, Long>
+{
+    Optional<TakingExam> getBySubjectAttendanceId(Long subjectAttendanceId);
+}
