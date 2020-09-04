c58962a0fb9ae3e96896f2368fdb182642b6418a
Lazar Filipovic <laki098@gmail.com>
2020-09-04 20:37:39 +0200

    Added fallback factory for faculty feign client

---
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/feign/hystrix/FacultyServiceFallbackFactory.java
@@ -0,0 +1,28 @@
+package com.ula.exam.feign.hystrix;
+
+import com.ula.exam.feign.FacultyFeignClient;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.ula.core.api.response.Response;
+
+@Component
+public class FacultyServiceFallbackFactory implements FallbackFactory<FacultyFeignClient>
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(FacultyServiceFallbackFactory.class);
+
+    @Override
+    public FacultyFeignClient create(Throwable cause)
+    {
+        return new FacultyFeignClient()
+        {
+            @Override
+            public Response<Object> getSubjectAttendanceById(Long id, String token)
+            {
+                LOGGER.error("Error has occurred while fetching subject attendance", cause);
+                return Response.exception().setErrors("Error has occurred while fetching subject attendance");
+            }
+        };
+    }
+}
