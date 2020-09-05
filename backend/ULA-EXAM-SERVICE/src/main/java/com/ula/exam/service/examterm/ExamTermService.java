22f7ee4bac7ca0e4ecbe6fa65a8151bea26defed
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-05 20:21:32 +0200

    Added service for ExamTerm

---
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/service/examterm/ExamTermService.java
@@ -0,0 +1,15 @@
+package com.ula.exam.service.examterm;
+
+import com.ula.exam.dto.model.ExamTermDTO;
+import com.ula.exam.service.exception.ExamTermNotFoundException;
+
+import java.util.List;
+
+public interface ExamTermService
+{
+    List<ExamTermDTO> index();
+
+    ExamTermDTO show(Long id)
+    throws ExamTermNotFoundException;
+
+}
