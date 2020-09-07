3c6227d81bc71c814ea540391893b455e0d95f5b
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-07 21:46:24 +0200

    Moved excludes to @SpringBootApplication

--- backend/ULA-STATIC-CONTENT-SERVICE/src/main/java/com/ula/content/UlaStaticContentServiceApplication.java
+++ backend/ULA-STATIC-CONTENT-SERVICE/src/main/java/com/ula/content/UlaStaticContentServiceApplication.java
@@ -1,7 +1,6 @@
 package com.ula.content;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -10,10 +9,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+
 @EnableEurekaClient
 @EnableCircuitBreaker
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @ComponentScan({"com.ula.content","org.ula.core"})
 public class UlaStaticContentServiceApplication
 {
