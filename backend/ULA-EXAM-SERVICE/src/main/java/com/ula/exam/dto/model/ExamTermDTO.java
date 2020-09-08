bea608f3074e099f6678317a4683f91668da6faf
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-08 20:30:43 +0200

    Remvoed JsonFormat

--- backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/dto/model/ExamTermDTO.java
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/dto/model/ExamTermDTO.java
@@ -1,6 +1,5 @@
 package com.ula.exam.dto.model;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,8 +16,6 @@ public class ExamTermDTO
 
     private Long id;
     private String name;
-    @JsonFormat(pattern = "YYYY-MM-DD HH:MM:SS")
     private Date startDate;
-    @JsonFormat(pattern = "YYYY-MM-DD HH:MM:SS")
     private Date endDate;
 }
