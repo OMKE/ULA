a43e80d97306e878522db5ee6ba1ba2b3b3b5be7
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-08 20:30:21 +0200

    Added global dateformat

--- backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/configuration/CoreConfiguration.java
+++ backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/configuration/CoreConfiguration.java
@@ -1,7 +1,12 @@
 package com.ula.student.configuration;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -24,5 +29,20 @@ public class CoreConfiguration implements WebMvcConfigurer
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer)
     {
         configurer.defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void configureMessageConverters
+    (
+            List<HttpMessageConverter<?>> converters
+    )
+    {
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
+
     }
 }
