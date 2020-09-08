fd475699e976e7759b770f33df45528cac624707
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-08 20:31:34 +0200

    Added global exception handlers

---
+++ backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/service/exception/handler/NotAuthenticatedExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.ula.student.service.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class NotAuthenticatedExceptionHandler extends org.ula.core.exception.handler.NotAuthenticatedExceptionHandler
+{
+}
---
+++ backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/service/exception/handler/RequiredRequestParamExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ula.student.service.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.ula.core.exception.handler.RequiredRequestParameterExceptionHandler;
+
+@RestControllerAdvice
+public class RequiredRequestParamExceptionHandler extends RequiredRequestParameterExceptionHandler
+{
+}
