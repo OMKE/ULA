ca48e26f31d0734f776119b2af3ef1eb940acd91
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-09 20:19:24 +0200

    Added UserUtil to exam service

---
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/util/UserUtil.java
@@ -0,0 +1,27 @@
+package com.ula.exam.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.ula.core.util.JWT;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class UserUtil
+{
+
+    @Autowired
+    private HttpServletRequest request;
+
+    public String getUsername()
+    {
+        JWT jwt = new JWT(request.getHeader("Authorization"));
+
+        return jwt.getUsername();
+    }
+
+    public String getToken()
+    {
+        return this.request.getHeader("Authorization");
+    }
+}
