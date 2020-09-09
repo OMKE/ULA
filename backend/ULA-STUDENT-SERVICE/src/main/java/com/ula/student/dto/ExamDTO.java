4f320c70fae8831a012526b281bd3b58091fc0cf
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-09 20:24:15 +0200

    Removed JsonFormat from date fields

--- backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/dto/ExamDTO.java
+++ backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/dto/ExamDTO.java
@@ -1,6 +1,5 @@
 package com.ula.student.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,9 +16,7 @@ public class ExamDTO
     private Long id;
     private Long examTypeId;
     private Long takingExamId;
-    @JsonFormat(pattern = "YYYY-MM-DD HH:MM:SS")
     private Date startTime;
-    @JsonFormat(pattern = "YYYY-MM-DD HH:MM:SS")
     private Date endTime;
     private double points;
     private String examType;
