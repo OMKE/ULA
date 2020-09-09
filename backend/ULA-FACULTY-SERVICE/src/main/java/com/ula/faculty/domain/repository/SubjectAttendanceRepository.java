d09c85d4767d79a5fd91d1d454873917fc0eabd6
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-09 16:08:11 +0200

    Added queries for getting student attendances by student id

--- backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/domain/repository/SubjectAttendanceRepository.java
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/domain/repository/SubjectAttendanceRepository.java
@@ -4,8 +4,12 @@ import com.ula.faculty.domain.model.SubjectAttendance;
 import org.springframework.stereotype.Repository;
 import org.ula.core.domain.repository.CrudRepository;
 
+import java.util.List;
+
 @Repository
 public interface SubjectAttendanceRepository extends CrudRepository<SubjectAttendance, Long>
 {
-
+    List<SubjectAttendance> getAllByStudentId(Long studentId);
+    List<SubjectAttendance> getAllByStudentIdAndFinalGradeNotNull(Long studentId);
+    List<SubjectAttendance> getAllByStudentIdAndFinalGradeNull(Long studentId);
 }
