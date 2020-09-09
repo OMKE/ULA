ecbdcc753df39e906ad92cbf5434467157356c72
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-09 20:23:32 +0200

    Added get request for getting subject attendance by id

--- backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/feign/FacultyService.java
+++ backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/feign/FacultyService.java
@@ -2,6 +2,7 @@ package com.ula.student.feign;
 
 import com.ula.student.dto.StudentOnYearDTO;
 import com.ula.student.dto.SubjectAttendanceWithSubjectDTO;
+import com.ula.student.feign.hystrix.FacultyServiceFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
-@FeignClient(value = "ula-faculty-service")
+@FeignClient(value = "ula-faculty-service", fallbackFactory = FacultyServiceFallbackFactory.class)
 public interface FacultyService
 {
 
@@ -24,4 +25,12 @@ public interface FacultyService
 
     @GetMapping("/private/subject-attendance/student/{id}/not-passed")
     List<SubjectAttendanceWithSubjectDTO> getNotPassedSubjectAttendancesWithSubjects(@RequestHeader("Authorization") String token, @PathVariable("id") Long studentOnYearId);
+
+    @GetMapping("/private/subject-attendance/student/{studentId}/{id}")
+    SubjectAttendanceWithSubjectDTO getSubjectAttendanceById
+            (
+            @RequestHeader("Authorization") String token,
+            @PathVariable("studentId") Long studentId,
+            @PathVariable("id") Long id
+            );
 }
