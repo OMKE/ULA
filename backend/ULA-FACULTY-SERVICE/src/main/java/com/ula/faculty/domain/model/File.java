ebc293fd4bfe49ffd1f0f5e17f73d62851cc7616
Lazar Filipovic <laki098@gmail.com>
2020-09-11 01:25:39 +0200

    Added File entity

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/domain/model/File.java
@@ -0,0 +1,28 @@
+package com.ula.faculty.domain.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.ula.core.domain.model.BaseEntity;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@Entity
+public class File extends BaseEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(256)")
+    private String path;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(256)")
+    private String description;
+
+}
