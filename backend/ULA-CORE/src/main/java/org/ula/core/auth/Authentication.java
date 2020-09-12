5f577d465bafdaac59a8d80e5b34d272c4969659
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-12 19:44:37 +0200

    Refactoring Authentication

--- backend/ULA-CORE/src/main/java/org/ula/core/auth/Authentication.java
+++ backend/ULA-CORE/src/main/java/org/ula/core/auth/Authentication.java
@@ -5,6 +5,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Component;
@@ -13,8 +15,8 @@ import org.ula.core.domain.model.ULAUserDTO;
 import org.ula.core.exception.JWTNotFoundException;
 import org.ula.core.exception.NotAuthorizedException;
 import org.ula.core.feign.AuthServiceFeignClient;
-import org.ula.core.util.JWT;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 
 @Aspect
@@ -23,13 +25,17 @@ import java.lang.reflect.Method;
 public class Authentication
 {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(Authentication.class);
+
     @Autowired
     private AuthServiceFeignClient authServiceFeignClient;
+
+    @Autowired
+    private HttpServletRequest request;
 
     @Before("@annotation(org.ula.core.annotation.Authorized)")
     public void authenticate(JoinPoint joinPoint) throws NotAuthorizedException, JWTNotFoundException
     {
-        JWT jwt = null;
 
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         Method method = signature.getMethod();
@@ -38,40 +44,35 @@ public class Authentication
         // Getting value from annotation, if there are many roles, they should be comma separated and in brackets
         String roles = authorizedAnnotation.value();
 
+        String token = this.request.getHeader("Authorization");
 
-
-        for(Object o: joinPoint.getArgs())
+        if(this.checkHeader(token))
         {
-            // If o is instance of JWT, we know that we found correct parameter
-            if(o instanceof JWT)
-            {
-                jwt = (JWT) o;
-            }
-        }
+            ULAUserDTO user = authServiceFeignClient.getUser(token, roles);
 
-        // If there is token present, we will call the authentication service to check the token
-
-        if(jwt != null)
-        {
-            // Check if there is no Authorization header present
-            if(jwt.getContent() == null || jwt.getContent().equals(""))
+            // If we didn't get the user object, that means that user is not authenticated so
+            // we throw an exception which we can catch with global ControllerAdvice
+            if (user == null)
             {
+                LOGGER.error("User is not authorized because AuthService returned null");
                 throw new NotAuthorizedException("User is not authorized");
-            } else {
-                // Auth-service has an util RoleResolver class which will create an array based on sent string
-                // If user has correct roles, auth service will return an user object
-                ULAUserDTO user = authServiceFeignClient.getUser(jwt.getContent(), roles);
-
-                // If we didn't get the user object, that means that user is not authenticated so
-                // we throw an exception which we can catch with global ControllerAdvice
-                if (user == null)
-                {
-                    throw new NotAuthorizedException("User is not authorized");
-                }
-
             }
         } else {
-            throw new JWTNotFoundException("@Authorized annotation can't be used without @Token parameter");
+            LOGGER.error("Missing Authorization header");
+            throw new NotAuthorizedException("User is not authorized");
+        }
+
+
+    }
+
+
+    private boolean checkHeader(String header)
+    {
+        if(header != null && header.equals(""))
+        {
+            return true;
+        } else {
+            return false;
         }
     }
 
