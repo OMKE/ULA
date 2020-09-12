dd72f230f4eb6437f624aeea7c9041d950f95a4c
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-12 22:01:05 +0200

    Added foreign key to TeachingMaterial and name field

--- backend/ULA-FACULTY-SERVICE/src/main/resources/db/migration/V1__DDL.sql
+++ backend/ULA-FACULTY-SERVICE/src/main/resources/db/migration/V1__DDL.sql
@@ -292,11 +292,14 @@ CREATE TABLE `teaching_term_outcome` (
 
 CREATE TABLE `teaching_material` (
     `id` bigint(20) not null auto_increment,
+    `teaching_term_id` bigint(20) not null ,
+    `name` varchar(128) not null ,
     `publication_date` date not null ,
     `created_at` timestamp not null,
     `deleted` tinyint(1) default '0',
     `updated_at` timestamp null default null,
-    primary key (`id`)
+    primary key (`id`),
+    foreign key (`teaching_term_id`) references `teaching_term` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `teaching_material_author` (
