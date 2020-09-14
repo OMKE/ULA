2d2a94e495abba53a701d7b64f18a350e0aab9cf
Lazar Filipovic <laki098@gmail.com>
2020-09-15 00:25:22 +0200

    Added DTO for SubjectNotification

---
+++ backend/ULA-STUDENT-SERVICE/src/main/java/com/ula/student/dto/SubjectNotificationDTO.java
@@ -0,0 +1,21 @@
+package com.ula.student.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+public class SubjectNotificationDTO
+{
+    private Long id;
+    private String subjectName;
+    private String type;
+    private Date time;
+    private String text;
+}
