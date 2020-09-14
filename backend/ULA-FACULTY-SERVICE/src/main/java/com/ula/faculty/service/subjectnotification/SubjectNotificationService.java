880a68196075ecf370f30cd3696f4b5f959f2d20
Lazar Filipovic <laki098@gmail.com>
2020-09-15 00:20:04 +0200

    Added SubjectNotificationService

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/service/subjectnotification/SubjectNotificationService.java
@@ -0,0 +1,28 @@
+package com.ula.faculty.service.subjectnotification;
+
+import com.ula.faculty.api.v1.request.StoreAndUpdateSubjectNotificationRequest;
+import com.ula.faculty.dto.model.SubjectNotificationDTO;
+import com.ula.faculty.service.exception.*;
+import org.springframework.data.domain.Pageable;
+import org.ula.core.exception.NotAuthorizedException;
+
+import java.util.List;
+
+public interface SubjectNotificationService
+{
+    List<SubjectNotificationDTO> index(Long subjectId, Pageable pageable)
+    throws SubjectRealizationNotFoundException;
+
+    SubjectNotificationDTO show(Long id)
+    throws SubjectNotificationNotFoundException;
+
+    List<SubjectNotificationDTO> getByStudentId(Long studentId, Pageable pageable)
+    throws StudentOnYearNotFoundException;
+
+    String store(Long subjectId,StoreAndUpdateSubjectNotificationRequest storeRequest)
+    throws SubjectRealizationNotFoundException, TeacherOnRealizationNotFoundException, SubjectNotificationTypeNotFoundException, NotAuthorizedException;
+
+    String update(Long id, StoreAndUpdateSubjectNotificationRequest updateRequest);
+
+    String delete(Long id);
+}
