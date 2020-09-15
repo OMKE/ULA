be47db6f8a2dbea9cff9d51ad9a9661ade7af5cb
Lazar Filipovic <laki098@gmail.com>
2020-09-15 20:05:57 +0200

    Added exception class

---
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/service/exception/TeacherSubjectRealizationNotFoundException.java
@@ -0,0 +1,14 @@
+package com.ula.faculty.service.exception;
+
+public class TeacherSubjectRealizationNotFoundException extends Exception
+{
+    public TeacherSubjectRealizationNotFoundException(String message)
+    {
+        super(message);
+    }
+
+    public TeacherSubjectRealizationNotFoundException(Throwable throwable)
+    {
+        super(throwable);
+    }
+}
