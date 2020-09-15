e39cd5939c3625f760b17caf468f955198505e45
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-15 20:10:35 +0200

    Added methods for getting student and all students based on array of ids

--- backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/feign/AuthServiceFeignClient.java
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/feign/AuthServiceFeignClient.java
@@ -1,10 +1,12 @@
 package com.ula.faculty.feign;
 
 import com.ula.faculty.domain.model.TeacherDTO;
+import com.ula.faculty.dto.model.StudentDTO;
 import com.ula.faculty.feign.hystrix.AuthServiceFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @FeignClient(value = "ula-auth-service", fallbackFactory = AuthServiceFallbackFactory.class)
 public interface AuthServiceFeignClient
@@ -12,4 +14,11 @@ public interface AuthServiceFeignClient
     // Returns currently logged teacher
     @GetMapping("/private/teacher")
     TeacherDTO getTeacher(@RequestHeader(value = "Authorization", required = true) String token);
+
+    @GetMapping("/private/student/username/{username}")
+    StudentDTO getStudent(@RequestHeader("Authorization") String token, @PathVariable("username") String username);
+
+    // returns all students with ids passed as array
+    @PostMapping("/private/student-all")
+    List<StudentDTO> getAllStudents(@RequestHeader("Authorization") String token, @RequestBody List<Long> ids);
 }
