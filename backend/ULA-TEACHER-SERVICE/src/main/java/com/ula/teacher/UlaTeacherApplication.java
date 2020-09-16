42c54b99d4cd7957e080fb94bd257ccdc14577a2
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-16 22:47:35 +0200

    Added Teacher service as proxy/composite service

---
+++ backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/UlaTeacherApplication.java
@@ -0,0 +1,15 @@
+package com.ula.teacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlaTeacherApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(UlaTeacherApplication.class, args);
+    }
+
+}
---
+++ backend/ULA-TEACHER-SERVICE/src/test/java/com/ula/teacher/UlaTeacherApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ula.teacher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlaTeacherApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
