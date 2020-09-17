0f0726011c32e5874cee4e81c0c39737eaa1ad11
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-17 22:02:36 +0200

    Added request files

---
+++ backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/api/v1/request/StoreAndUpdateSubjectNotificationRequest.java
@@ -0,0 +1,34 @@
+package com.ula.teacher.api.v1.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StoreAndUpdateSubjectNotificationRequest
+{
+    @NotNull(message = "Subject realization id is required")
+    @Min(value = 1, message = "Subject realization id can not be lower than 1")
+    private Long subjectRealizationId;
+
+
+    @NotNull(message = "Type id is required")
+    @Min(value = 1, message = "Type id can not be lower than 1")
+    private Long typeId;
+
+    @NotEmpty(message = "Notification message is required")
+    @Size(min = 10, message = "Notification message must be at least 10 characters long")
+    @Size(max = 64, message = "Notification message can not be longer than 64 characters")
+    private String text;
+}
---
+++ backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/api/v1/request/StoreTeachingMaterialAuthorRequest.java
@@ -0,0 +1,30 @@
+package com.ula.teacher.api.v1.request;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StoreTeachingMaterialAuthorRequest
+{
+
+    @NotEmpty(message = "First name is required")
+    @Size(min = 3, message = "First name has to be at least 3 characters long")
+    @Size(max = 128, message = "First name can not be longer than 128 charcaters")
+    private String firstName;
+
+    @NotEmpty(message = "Last name is required")
+    @Size(min = 3, message = "Last name has to be at least 3 characters long")
+    @Size(max = 128, message = "Last name can not be longer than 128 charcaters")
+    private String lastName;
+
+}
