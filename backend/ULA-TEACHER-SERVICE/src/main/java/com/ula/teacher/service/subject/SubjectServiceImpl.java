4fff6fc2aedca6155cf0fd1da659afbfd8b05776
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-18 18:36:21 +0200

    Added Subject Service

---
+++ backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/service/subject/SubjectServiceImpl.java
@@ -0,0 +1,60 @@
+package com.ula.teacher.service.subject;
+
+import com.ula.teacher.domain.model.TeacherDTO;
+import com.ula.teacher.dto.SubjectWithRealizationIdDTO;
+import com.ula.teacher.feign.AuthServiceFeignClient;
+import com.ula.teacher.feign.FacultyServiceFeignClient;
+import com.ula.teacher.service.exception.SubjectWithRealizationIdNotFoundException;
+import com.ula.teacher.service.exception.TeacherNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.ula.core.util.JWTUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SubjectServiceImpl implements SubjectService
+{
+
+    @Autowired
+    private AuthServiceFeignClient authService;
+
+    @Autowired
+    private FacultyServiceFeignClient facultyService;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Override
+    public List<SubjectWithRealizationIdDTO> index()
+    {
+        TeacherDTO teacherDTO = this.authService.getTeacher(jwtUtil.getToken());
+        if(teacherDTO != null)
+        {
+            return this.facultyService.getSubjects(jwtUtil.getToken(), teacherDTO.getId());
+        } else {
+            return new ArrayList<>();
+        }
+
+    }
+
+    @Override
+    public SubjectWithRealizationIdDTO show(Long id)
+    throws SubjectWithRealizationIdNotFoundException, TeacherNotFoundException
+    {
+        TeacherDTO teacherDTO = this.authService.getTeacher(jwtUtil.getToken());
+        if(teacherDTO != null)
+        {
+            SubjectWithRealizationIdDTO subject = this.facultyService.getSubjectById(jwtUtil.getToken(), teacherDTO.getId(), id);
+            if(subject != null)
+            {
+                return subject;
+            } else {
+                throw new SubjectWithRealizationIdNotFoundException(String.format("Subject with id: %s could not be found", id));
+            }
+        } else {
+            throw new TeacherNotFoundException(String.format("Not Authorized"));
+        }
+    }
+}
