7855a0e2a197391f0d2a919328ab63ebf6d28bec
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-20 00:18:17 +0200

    Modified StudentService

--- backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/service/student/StudentService.java
+++ backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/service/student/StudentService.java
@@ -1,11 +1,20 @@
 package com.ula.teacher.service.student;
 
-import com.ula.teacher.dto.StudentDTO;
+import com.ula.teacher.dto.StudentOnYearDTO;
+import com.ula.teacher.service.exception.StudentOnYearNotFoundException;
+import com.ula.teacher.service.exception.TeacherNotFoundException;
 import org.springframework.data.domain.Pageable;
+import org.ula.core.api.response.Response;
 
 import java.util.List;
 
 public interface StudentService
 {
-    List<StudentDTO> getStudentsBySubjectId(Long subjectId, Pageable pageable);
+    List<StudentOnYearDTO> index(Pageable pageable)
+    throws TeacherNotFoundException;
+    List<StudentOnYearDTO> getStudentsBySubjectId(Long subjectId, Pageable pageable);
+    StudentOnYearDTO show(Long studentId)
+    throws StudentOnYearNotFoundException;
+
+    Response<Object> search(String searchParam);
 }
