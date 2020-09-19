e23533dc31bc4ed409799831b8bc5130c40b6e3d
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-20 00:25:59 +0200

    Added search query for getting User by First name, Last name or username

--- backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/domain/repository/UserRepository.java
+++ backend/ULA-AUTHENTICATION/src/main/java/com/ula/authentication/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.ula.authentication.domain.model.User;
 import org.springframework.stereotype.Repository;
 import org.ula.core.domain.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -21,5 +22,7 @@ public interface UserRepository extends CrudRepository<User, Long>
 	Optional<User> findByEmail(String email);
 
 	Optional<User> findByEmailAndPasswordAndDeletedFalse(String email, String password);
+
+	List<User> findAllByFirstNameContainsOrLastNameContainsOrUsernameContains(String firstName, String lastName, String username);
 
 }
