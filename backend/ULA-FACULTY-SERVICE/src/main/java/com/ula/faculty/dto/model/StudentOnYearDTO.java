ce08198da36d281e1076074b8b6502298e82d7c4
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-20 00:21:24 +0200

    Added StudentDTO to studentOnYear

--- backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/dto/model/StudentOnYearDTO.java
+++ backend/ULA-FACULTY-SERVICE/src/main/java/com/ula/faculty/dto/model/StudentOnYearDTO.java
@@ -1,5 +1,6 @@
 package com.ula.faculty.dto.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -12,10 +13,12 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @Accessors(chain = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class StudentOnYearDTO
 {
     private Long id;
     private Long studentId;
+    private StudentDTO student;
     private List<Long> yearOfStudyIds;
     private Date dateOfEnrollment;
     private String transcriptIdentifier;
