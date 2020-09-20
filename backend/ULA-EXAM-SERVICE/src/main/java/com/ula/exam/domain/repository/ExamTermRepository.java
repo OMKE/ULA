a33e5f02bdf3669a78caf30da19b71d470678a72
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-20 18:53:13 +0200

    Added query for getting latest Teaching Term

--- backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/domain/repository/ExamTermRepository.java
+++ backend/ULA-EXAM-SERVICE/src/main/java/com/ula/exam/domain/repository/ExamTermRepository.java
@@ -4,8 +4,10 @@ import com.ula.exam.domain.model.ExamTerm;
 import org.springframework.stereotype.Repository;
 import org.ula.core.domain.repository.CrudRepository;
 
+import java.util.Date;
+
 @Repository
 public interface ExamTermRepository extends CrudRepository<ExamTerm, Long>
 {
-
+    ExamTerm findTopByStartDateLessThanEqualOrderByStartDateDesc(Date date);
 }
