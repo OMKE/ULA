3cc6d76f5e5ad881a98017b208c52b46b175b84a
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-20 18:58:43 +0200

    Added method for updating final grade

--- backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/service/subject/SubjectServiceImpl.java
+++ backend/ULA-TEACHER-SERVICE/src/main/java/com/ula/teacher/service/subject/SubjectServiceImpl.java
@@ -1,13 +1,18 @@
 package com.ula.teacher.service.subject;
 
+import com.ula.teacher.api.v1.request.UpdateSubjectAttendanceFinalGradeRequest;
 import com.ula.teacher.domain.model.TeacherDTO;
+import com.ula.teacher.dto.StudentOnYearDTO;
 import com.ula.teacher.dto.SubjectWithRealizationIdDTO;
 import com.ula.teacher.feign.AuthServiceFeignClient;
 import com.ula.teacher.feign.FacultyServiceFeignClient;
+import com.ula.teacher.service.exception.StudentOnYearNotFoundException;
 import com.ula.teacher.service.exception.SubjectWithRealizationIdNotFoundException;
 import com.ula.teacher.service.exception.TeacherNotFoundException;
+import com.ula.teacher.service.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.ula.core.api.response.Response;
 import org.ula.core.util.JWTUtil;
 
 import java.util.ArrayList;
@@ -22,6 +27,9 @@ public class SubjectServiceImpl implements SubjectService
 
     @Autowired
     private FacultyServiceFeignClient facultyService;
+
+    @Autowired
+    private StudentService studentService;
 
     @Autowired
     private JWTUtil jwtUtil;
@@ -55,6 +63,26 @@ public class SubjectServiceImpl implements SubjectService
             }
         } else {
             throw new TeacherNotFoundException(String.format("Not Authorized"));
+        }
+    }
+
+    @Override
+    public Response<Object> updateFinalGrade
+    (
+            Long subjectId,
+            Long studentId,
+            UpdateSubjectAttendanceFinalGradeRequest request
+    )
+    throws StudentOnYearNotFoundException, TeacherNotFoundException
+    {
+        StudentOnYearDTO student = this.studentService.showInSubject(subjectId, studentId);
+
+        TeacherDTO teacherDTO = this.authService.getTeacher(this.jwtUtil.getToken());
+        if(teacherDTO != null)
+        {
+            return this.facultyService.updateFinalGrade(this.jwtUtil.getToken(), teacherDTO.getId(), student.getAttendance().getId(), request);
+        } else {
+            throw new TeacherNotFoundException("Not authorized");
         }
     }
 }
