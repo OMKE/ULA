850556d94d08311c43c22dce4d44edfcf10ba94d
Lazar Filipovic <laki098@gmail.com>
2020-09-23 12:07:41 +0200

    Added student exams controller

---
+++ frontend/projects/main-app/src/app/dashboard/student/student-exams/student-exams.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-exams',
+  templateUrl: './student-exams.component.html',
+  styleUrls: ['./student-exams.component.scss']
+})
+export class StudentExamsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
