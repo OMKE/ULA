eebf823b4924bcb4e4f1e7ccb6b6f1be5006a53d
Lazar Filipovic <laki098@gmail.com>
2020-09-23 12:06:53 +0200

    Added student routing module

---
+++ frontend/projects/main-app/src/app/dashboard/student/student-routing.module.ts
@@ -0,0 +1,62 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { StudentChangePasswordComponent } from "./student-change-password/student-change-password.component";
+import { StudentChangeProfileImageComponent } from "./student-change-profile-image/student-change-profile-image.component";
+import { StudentExamsComponent } from "./student-exams/student-exams.component";
+import { StudentHistoryComponent } from "./student-history/student-history.component";
+import { StudentNotificationsComponent } from "./student-notifications/student-notifications.component";
+import { StudentProfileComponent } from "./student-profile/student-profile.component";
+import { StudentSubjectsComponent } from "./student-subjects/student-subjects.component";
+import { StudentUpdateProfileComponent } from "./student-update-profile/student-update-profile.component";
+import { StudentComponent } from "./student.component";
+
+const routes: Routes = [
+    {
+        path: "",
+        component: StudentComponent,
+        children: [
+            {
+                path: "",
+                component: StudentProfileComponent,
+            },
+            {
+                path: "update-profile",
+                component: StudentUpdateProfileComponent,
+            },
+            {
+                path: "change-password",
+                component: StudentChangePasswordComponent,
+            },
+            {
+                path: "profile-image",
+                component: StudentChangeProfileImageComponent,
+            },
+            {
+                path: "subjects",
+                component: StudentSubjectsComponent,
+            },
+            {
+                path: "notifications",
+                component: StudentNotificationsComponent,
+            },
+            {
+                path: "history",
+                component: StudentHistoryComponent,
+            },
+            {
+                path: "exams",
+                component: StudentExamsComponent,
+            },
+        ],
+    },
+    {
+        path: "**",
+        component: StudentProfileComponent,
+    },
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+})
+export class StudentRoutingModule {}
