8e8a4382c12c898492d90847c6ed29d3fd9eef9e
Lazar Filipovic <laki098@gmail.com>
2020-09-23 03:30:03 +0200

    Added faculty history component

--- frontend/projects/main-app/src/app/components/faculties/faculty/faculty-about/faculty-history/faculty-history.component.ts
+++ frontend/projects/main-app/src/app/components/faculties/faculty/faculty-about/faculty-history/faculty-history.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
+import { Faculty } from "projects/main-app/src/app/core/models/Faculty";
+import { FacultyService } from "projects/main-app/src/app/services/faculty.service";
 
 @Component({
-  selector: 'app-faculty-history',
-  templateUrl: './faculty-history.component.html',
-  styleUrls: ['./faculty-history.component.scss']
+    selector: "app-faculty-history",
+    templateUrl: "./faculty-history.component.html",
+    styleUrls: ["./faculty-history.component.scss"],
+    encapsulation: ViewEncapsulation.None,
 })
 export class FacultyHistoryComponent implements OnInit {
+    constructor(
+        private facultyService: FacultyService,
+        private router: Router
+    ) {}
 
-  constructor() { }
+    faculty: Faculty = {
+        id: null,
+        name: "",
+        campusId: null,
+        icon: "",
+        contact: {
+            phoneNumber: "",
+            email: "",
+        },
+        location: {
+            id: null,
+            longitude: null,
+            latitude: null,
+            name: "",
+        },
+        information: {
+            facultyId: null,
+            history: "",
+            president: "",
+            mission: "",
+            accreditation: "",
+        },
+    };
 
-  ngOnInit(): void {
-  }
+    content: string = "";
 
+    heading: string = "";
+
+    ngOnInit(): void {
+        const url = this.router.url.split("/")[2];
+        this.facultyService.getFaculty(url).subscribe((response) => {
+            this.faculty = response.payload;
+            this.content = this.faculty.information.history;
+            this.heading = this.faculty.name;
+        });
+    }
 }
