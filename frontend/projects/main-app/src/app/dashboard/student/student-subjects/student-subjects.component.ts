582242b11ef2352dc1bd6aacf7418ff5ea8b4236
Lazar Filipovic <laki098@gmail.com>
2020-09-23 12:06:37 +0200

    Added student subjects component

---
+++ frontend/projects/main-app/src/app/dashboard/student/student-subjects/student-subjects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-subjects',
+  templateUrl: './student-subjects.component.html',
+  styleUrls: ['./student-subjects.component.scss']
+})
+export class StudentSubjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
