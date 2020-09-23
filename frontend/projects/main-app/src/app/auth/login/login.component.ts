681234aca61ffad15223140d7d861290e3e62cfb
Lazar Filipovic <laki098@gmail.com>
2020-09-23 03:31:37 +0200

    Added login component to auth module

---
+++ frontend/projects/main-app/src/app/auth/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { User } from "../../core/models/User";
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.scss"],
+})
+export class LoginComponent implements OnInit {
+    constructor(private authService: AuthService, private router: Router) {}
+
+    user: User = {
+        username: "",
+        password: "",
+        roles: [],
+    };
+
+    ngOnInit(): void {}
+
+    errors: string[];
+
+    login() {
+        this.authService.login(this.user).subscribe((response) => {
+            if (response.errors != undefined) {
+                this.errors = response.errors;
+            } else {
+                this.router.navigate(["dashboard"]);
+            }
+        });
+    }
+}
