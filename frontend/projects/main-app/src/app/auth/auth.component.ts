96782e0f3bdebf6d886583417562969a2c8596b5
Lazar Filipovic <laki098@gmail.com>
2020-09-23 12:09:55 +0200

    Added redirection to dashboard if user is logged in

--- frontend/projects/main-app/src/app/auth/auth.component.ts
+++ frontend/projects/main-app/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 
 @Component({
     selector: "app-auth",
@@ -7,14 +8,18 @@ import { Router } from "@angular/router";
     styleUrls: ["./auth.component.scss"],
 })
 export class AuthComponent implements OnInit {
-    constructor(private router: Router) {}
+    constructor(private router: Router, private authService: AuthService) {}
 
     ngOnInit(): void {
         if (
             this.router.url.split("/")[1] == "auth" &&
             this.router.url.split("/").length == 2
         ) {
-            this.router.navigate(["auth/login"]);
+            if (this.authService.getUser()) {
+                this.router.navigate(["dashboard/user"]);
+            } else {
+                this.router.navigate(["auth/login"]);
+            }
         }
     }
 }
