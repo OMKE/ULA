74eae88fd9f808d9285a6d9a1435ed2d1acbd5e1
Lazar Filipovic <laki098@gmail.com>
2020-09-23 03:31:45 +0200

    Updated routes

--- frontend/projects/main-app/src/app/app-routing.module.ts
+++ frontend/projects/main-app/src/app/app-routing.module.ts
@@ -1,42 +1,56 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ContactComponent } from "./components/contact/contact.component";
 import { IndexComponent } from "./components/index/index.component";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: IndexComponent,
-    data: {
-      title: "University of Los Angeles",
+    {
+        path: "",
+        component: IndexComponent,
+        data: {
+            title: "University of Los Angeles",
+        },
     },
-  },
-  {
-    path: "about",
-    data: {
-      title: "ULA - About university",
+    {
+        path: "about",
+        data: {
+            title: "ULA - About university",
+        },
+        loadChildren: () =>
+            import("./components/about/about.module").then(
+                (m) => m.AboutModule
+            ),
     },
-    loadChildren: () =>
-      import("./components/about/about.module").then((m) => m.AboutModule),
-  },
-  {
-    path: "faculties",
-    data: {
-      title: "ULA - Faculties",
+    {
+        path: "faculties",
+        data: {
+            title: "ULA - Faculties",
+        },
+        loadChildren: () =>
+            import("./components/faculties/faculties.module").then(
+                (m) => m.FacultiesModule
+            ),
+    },
+    {
+        path: "contact",
+        component: ContactComponent,
+    },
     {
         path: "auth",
         loadChildren: () =>
             import("./auth/auth.module").then((m) => m.AuthModule),
     },
+    {
+        path: "dashboard",
+        loadChildren: () =>
+            import("./dashboard/dashboard.module").then(
+                (m) => m.DashboardModule
+            ),
     },
-    loadChildren: () =>
-      import("./components/faculties/faculties.module").then(
-        (m) => m.FacultiesModule
-      ),
-  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
