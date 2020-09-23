45e446f30e0d27bf6c658ba00ab5ad6a0e475cc5
Lazar Filipovic <laki098@gmail.com>
2020-09-23 03:29:52 +0200

    Added faculty mission component

--- frontend/projects/main-app/src/app/components/faculties/faculty/faculty-about/faculty-mission/faculty-mission.component.ts
+++ frontend/projects/main-app/src/app/components/faculties/faculty/faculty-about/faculty-mission/faculty-mission.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
+import { Faculty } from "projects/main-app/src/app/core/models/Faculty";
+import { FacultyService } from "projects/main-app/src/app/services/faculty.service";
 
 @Component({
-  selector: 'app-faculty-mission',
-  templateUrl: './faculty-mission.component.html',
-  styleUrls: ['./faculty-mission.component.scss']
+    selector: "app-faculty-mission",
+    templateUrl: "./faculty-mission.component.html",
+    styleUrls: ["./faculty-mission.component.scss"],
+    encapsulation: ViewEncapsulation.None,
 })
 export class FacultyMissionComponent implements OnInit {
+    constructor(
+        private facultyService: FacultyService,
+        private router: Router
+    ) {}
 
-  constructor() { }
+    faculty: Faculty = {
+        id: null,
+        name: "",
+        campusId: null,
+        icon: "",
+        contact: {
+            phoneNumber: "",
+            email: "",
+        },
+        location: {
+            id: null,
+            longitude: null,
+            latitude: null,
+            name: "",
+        },
+        information: {
+            facultyId: null,
+            history: "",
+            president: "",
+            mission: "",
+            accreditation: "",
+        },
+    };
 
-  ngOnInit(): void {
-  }
+    content: string = "";
 
+    ngOnInit(): void {
+        const url = this.router.url.split("/")[2];
+        this.facultyService.getFaculty(url).subscribe((response) => {
+            this.faculty = response.payload;
+            this.content = this.faculty.information.mission;
+        });
+    }
 }
