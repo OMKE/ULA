ab36f1a17dcdb36100c457f404281bd9ba6819e8
Lazar Filipovic <laki098@gmail.com>
2020-09-23 12:07:31 +0200

    Added student history component

---
+++ frontend/projects/main-app/src/app/dashboard/student/student-history/student-history.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-history',
+  templateUrl: './student-history.component.html',
+  styleUrls: ['./student-history.component.scss']
+})
+export class StudentHistoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
