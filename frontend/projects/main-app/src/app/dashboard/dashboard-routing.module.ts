e8ee9dbeea10bf4b22ec48d4119189e1e8daf12d
Lazar Filipovic <laki098@gmail.com>
2020-09-23 12:08:19 +0200

    Added dashboard module

--- frontend/projects/main-app/src/app/dashboard/dashboard-routing.module.ts
+++ frontend/projects/main-app/src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AdminGuard } from "../guards/admin.guard";
+import { StudentGuard } from "../guards/student.guard";
+import { TeacherGuard } from "../guards/teacher.guard";
+import { UserGuard } from "../guards/user.guard";
 import { DashboardComponent } from "./dashboard.component";
 
 const routes: Routes = [
     {
         path: "",
+        redirectTo: "",
+        pathMatch: "full",
         component: DashboardComponent,
+    },
+    {
+        path: "user",
+        loadChildren: () =>
+            import("./user/user.module").then((m) => m.UserModule),
+        canActivate: [UserGuard],
+    },
+    {
+        path: "student",
+        loadChildren: () =>
+            import("./student/student.module").then((m) => m.StudentModule),
+        canActivate: [StudentGuard],
+    },
+    {
+        path: "admin",
+        loadChildren: () =>
+            import("./admin/admin.module").then((m) => m.AdminModule),
+        canActivate: [AdminGuard],
+    },
+    {
+        path: "teacher",
+        loadChildren: () =>
+            import("./teacher/teacher.module").then((m) => m.TeacherModule),
+        canActivate: [TeacherGuard],
     },
 ];
 
