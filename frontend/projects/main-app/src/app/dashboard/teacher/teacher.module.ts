9222701e916d5246ac96c354a32d1af8ee773f54
Lazar Filipovic <laki098@gmail.com>
2020-09-24 08:48:26 +0200

    Updated teacher module and component

--- frontend/projects/main-app/src/app/dashboard/teacher/teacher.module.ts
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher.module.ts
@@ -1,10 +1,48 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../../shared/shared.module";
 import { TeacherComponent } from "../teacher/teacher.component";
+import { TeacherAddTeachingTermComponent } from "./teacher-add-teaching-term/teacher-add-teaching-term.component";
+import { TeacherChangePasswordComponent } from "./teacher-change-password/teacher-change-password.component";
+import { TeacherChangeProfileImageComponent } from "./teacher-change-profile-image/teacher-change-profile-image.component";
+import { TeacherExamAddComponent } from "./teacher-exam-add/teacher-exam-add.component";
+import { TeacherExamUpdateComponent } from "./teacher-exam-update/teacher-exam-update.component";
+import { TeacherExamComponent } from "./teacher-exam/teacher-exam.component";
+import { TeacherNavbarComponent } from "./teacher-navbar/teacher-navbar.component";
+import { TeacherNotificationsComponent } from "./teacher-notifications/teacher-notifications.component";
+import { TeacherProfileComponent } from "./teacher-profile/teacher-profile.component";
 import { TeacherRoutingModule } from "./teacher-routing.module";
+import { TeacherStudentsComponent } from "./teacher-students/teacher-students.component";
+import { TeacherSubjectsComponent } from "./teacher-subjects/teacher-subjects.component";
+import { TeacherTeachingTermsComponent } from "./teacher-teaching-terms/teacher-teaching-terms.component";
+import { TeacherUpdateProfileComponent } from "./teacher-update-profile/teacher-update-profile.component";
+import { TeacherUpdateSubjectSyllabusComponent } from "./teacher-update-subject-syllabus/teacher-update-subject-syllabus.component";
+import { TeacherNotificationsAddComponent } from './teacher-notifications-add/teacher-notifications-add.component';
+import { TeacherStudentComponent } from './teacher-student/teacher-student.component';
+import { TeacherSubjectComponent } from './teacher-subject/teacher-subject.component';
 
 @NgModule({
-    declarations: [TeacherComponent],
-    imports: [CommonModule, TeacherRoutingModule],
+    declarations: [
+        TeacherComponent,
+        TeacherProfileComponent,
+        TeacherUpdateProfileComponent,
+        TeacherChangePasswordComponent,
+        TeacherChangeProfileImageComponent,
+        TeacherNotificationsComponent,
+        TeacherStudentsComponent,
+        TeacherSubjectsComponent,
+        TeacherUpdateSubjectSyllabusComponent,
+        TeacherTeachingTermsComponent,
+        TeacherAddTeachingTermComponent,
+        TeacherExamComponent,
+        TeacherExamUpdateComponent,
+        TeacherExamAddComponent,
+        TeacherNavbarComponent,
+        TeacherNotificationsAddComponent,
+        TeacherStudentComponent,
+        TeacherSubjectComponent,
+    ],
+    imports: [CommonModule, TeacherRoutingModule, FormsModule, SharedModule],
 })
 export class TeacherModule {}
