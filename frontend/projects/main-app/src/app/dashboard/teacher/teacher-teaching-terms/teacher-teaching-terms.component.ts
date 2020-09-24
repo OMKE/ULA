39c28365d6f833665834efbc141e35b7712f92f7
Lazar Filipovic <laki098@gmail.com>
2020-09-24 08:49:13 +0200

    Added teaching term component

---
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher-teaching-terms/teacher-teaching-terms.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-teaching-terms',
+  templateUrl: './teacher-teaching-terms.component.html',
+  styleUrls: ['./teacher-teaching-terms.component.scss']
+})
+export class TeacherTeachingTermsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
