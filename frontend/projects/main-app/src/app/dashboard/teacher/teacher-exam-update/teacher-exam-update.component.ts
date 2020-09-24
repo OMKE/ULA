4ab2d205862d52dc343b9cf0dfa637a8c7a3820b
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-24 14:20:23 +0200

    Updated teacher exam update component

--- frontend/projects/main-app/src/app/dashboard/teacher/teacher-exam-update/teacher-exam-update.component.ts
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher-exam-update/teacher-exam-update.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { TeacherExam } from "../../../core/models/TeacherExam";
+import { UpdateStudentExamRequest } from "../../../core/requests/UpdateStudentExamRequest";
+import { TeacherService } from "../../../services/teacher.service";
 
 @Component({
-  selector: 'app-teacher-exam-update',
-  templateUrl: './teacher-exam-update.component.html',
-  styleUrls: ['./teacher-exam-update.component.scss']
+    selector: "app-teacher-exam-update",
+    templateUrl: "./teacher-exam-update.component.html",
+    styleUrls: ["./teacher-exam-update.component.scss"],
 })
 export class TeacherExamUpdateComponent implements OnInit {
+    constructor(
+        private teacherService: TeacherService,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.getExam();
+    }
 
-  ngOnInit(): void {
-  }
+    request: UpdateStudentExamRequest = {
+        points: null,
+        description: "",
+    };
 
+    exam: TeacherExam = {
+        id: null,
+        examType: "",
+        takingExamId: null,
+        examTypeId: null,
+        points: null,
+        examEntry: {
+            id: null,
+            examTerm: null,
+            examTermId: null,
+            active: null,
+            examId: null,
+        },
+        startTime: null,
+        endTime: null,
+        finalExam: null,
+    };
+
+    getExam(): void {
+        this.teacherService
+            .getOneStudentExam(
+                this.route.snapshot.params["id"],
+                this.route.snapshot.params["studentId"],
+                this.route.snapshot.params["examId"]
+            )
+            .subscribe((res) => {
+                this.exam = res.payload;
+            });
+    }
+
+    errors: string[] = [];
+    updateExam(): void {
+        this.teacherService
+            .updateStudentExam(
+                this.route.snapshot.params["id"],
+                this.route.snapshot.params["studentId"],
+                this.route.snapshot.params["examId"],
+                this.request
+            )
+            .subscribe((res) => {
+                if (res.errors != undefined) {
+                    this.errors = res.errors;
+                } else {
+                    this.router.navigate([
+                        `/dashboard/teacher/subjects/${this.route.snapshot.params["id"]}/student/${this.route.snapshot.params["studentId"]}`,
+                    ]);
+                }
+            });
+    }
 }
