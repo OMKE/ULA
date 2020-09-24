17c3d83180afba97ec56eead8c7ef8bebdffa50d
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-24 08:52:53 +0200

    Updated student subjects component

--- frontend/projects/main-app/src/app/dashboard/student/student-subjects/student-subjects.component.ts
+++ frontend/projects/main-app/src/app/dashboard/student/student-subjects/student-subjects.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { YearOfStudy } from "../../../core/models/YearOfStudy";
+import { StudentService } from "../../../services/student.service";
 
 @Component({
-  selector: 'app-student-subjects',
-  templateUrl: './student-subjects.component.html',
-  styleUrls: ['./student-subjects.component.scss']
+    selector: "app-student-subjects",
+    templateUrl: "./student-subjects.component.html",
+    styleUrls: ["./student-subjects.component.scss"],
 })
 export class StudentSubjectsComponent implements OnInit {
+    constructor(private studentService: StudentService) {}
 
-  constructor() { }
+    ngOnInit(): void {
+        this.getYearOfStudies();
+    }
+    yearOfStudies: YearOfStudy[] = [];
 
-  ngOnInit(): void {
-  }
+    allTotalEspb: number = 0;
 
+    getYearOfStudies(): void {
+        this.studentService.getYearOfStudies().subscribe((res) => {
+            this.yearOfStudies = res.payload;
+            this.yearOfStudies.forEach(
+                (e) => (this.allTotalEspb += e.totalEspb)
+            );
+        });
+    }
 }
