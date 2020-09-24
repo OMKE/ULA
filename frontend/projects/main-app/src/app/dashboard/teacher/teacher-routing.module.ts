a54ca954ced40e1a4ed324c45687cf95c7a92797
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-24 14:19:55 +0200

    Added routes for teaching terms, teacher subject and exam components

--- frontend/projects/main-app/src/app/dashboard/teacher/teacher-routing.module.ts
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { TeacherAddTeachingTermComponent } from "./teacher-add-teaching-term/teacher-add-teaching-term.component";
 import { TeacherChangePasswordComponent } from "./teacher-change-password/teacher-change-password.component";
 import { TeacherChangeProfileImageComponent } from "./teacher-change-profile-image/teacher-change-profile-image.component";
+import { TeacherExamUpdateComponent } from "./teacher-exam-update/teacher-exam-update.component";
 import { TeacherNotificationsAddComponent } from "./teacher-notifications-add/teacher-notifications-add.component";
 import { TeacherNotificationsComponent } from "./teacher-notifications/teacher-notifications.component";
 import { TeacherProfileComponent } from "./teacher-profile/teacher-profile.component";
 import { TeacherStudentComponent } from "./teacher-student/teacher-student.component";
 import { TeacherStudentsComponent } from "./teacher-students/teacher-students.component";
+import { TeacherSubjectStudentComponent } from "./teacher-subject-student/teacher-subject-student.component";
 import { TeacherSubjectComponent } from "./teacher-subject/teacher-subject.component";
 import { TeacherSubjectsComponent } from "./teacher-subjects/teacher-subjects.component";
-import { TeacherTeachingTermsComponent } from "./teacher-teaching-terms/teacher-teaching-terms.component";
 import { TeacherUpdateProfileComponent } from "./teacher-update-profile/teacher-update-profile.component";
 import { TeacherComponent } from "./teacher.component";
 
@@ -43,8 +45,16 @@ const routes: Routes = [
                 component: TeacherSubjectComponent,
             },
             {
-                path: "teaching-terms",
-                component: TeacherTeachingTermsComponent,
+                path: "subjects/:id/add-teaching-term",
+                component: TeacherAddTeachingTermComponent,
+            },
+            {
+                path: "subjects/:id/student/:studentId",
+                component: TeacherSubjectStudentComponent,
+            },
+            {
+                path: "subjects/:id/student/:studentId/update-exam/:examId",
+                component: TeacherExamUpdateComponent,
             },
             {
                 path: "students",
