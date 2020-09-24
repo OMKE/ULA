1141d3d3f43d9b18ca1e7cef59bfeb7ddd0654bc
Lazar Filipovic <laki098@gmail.com>
2020-09-24 08:48:37 +0200

    Added subject syllabus component

---
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher-update-subject-syllabus/teacher-update-subject-syllabus.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-update-subject-syllabus',
+  templateUrl: './teacher-update-subject-syllabus.component.html',
+  styleUrls: ['./teacher-update-subject-syllabus.component.scss']
+})
+export class TeacherUpdateSubjectSyllabusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
