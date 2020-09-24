d68de0259d88816139a4be918b1b0130c27a0f42
Lazar Filipovic <laki098@gmail.com>
2020-09-24 08:51:19 +0200

    Added teacher exam add component

---
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher-exam-add/teacher-exam-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-exam-add',
+  templateUrl: './teacher-exam-add.component.html',
+  styleUrls: ['./teacher-exam-add.component.scss']
+})
+export class TeacherExamAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
