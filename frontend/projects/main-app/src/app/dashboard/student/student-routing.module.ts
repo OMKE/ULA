c1c2cbad4537a56fe7e500310aef369cbec96bc3
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-24 08:53:37 +0200

    Added exams path

--- frontend/projects/main-app/src/app/dashboard/student/student-routing.module.ts
+++ frontend/projects/main-app/src/app/dashboard/student/student-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { StudentChangePasswordComponent } from "./student-change-password/student-change-password.component";
 import { StudentChangeProfileImageComponent } from "./student-change-profile-image/student-change-profile-image.component";
-import { StudentExamsComponent } from "./student-exams/student-exams.component";
 import { StudentHistoryComponent } from "./student-history/student-history.component";
 import { StudentNotificationsComponent } from "./student-notifications/student-notifications.component";
 import { StudentProfileComponent } from "./student-profile/student-profile.component";
+import { StudentSubjectComponent } from "./student-subject/student-subject.component";
 import { StudentSubjectsComponent } from "./student-subjects/student-subjects.component";
 import { StudentUpdateProfileComponent } from "./student-update-profile/student-update-profile.component";
 import { StudentComponent } from "./student.component";
@@ -36,16 +36,16 @@ const routes: Routes = [
                 component: StudentSubjectsComponent,
             },
             {
+                path: "subjects/:id",
+                component: StudentSubjectComponent,
+            },
+            {
                 path: "notifications",
                 component: StudentNotificationsComponent,
             },
             {
                 path: "history",
                 component: StudentHistoryComponent,
-            },
-            {
-                path: "exams",
-                component: StudentExamsComponent,
             },
         ],
     },
