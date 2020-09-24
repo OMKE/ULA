badd1cb02e08f5cc60dcb8b94e598319ddc9ba92
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-24 08:54:10 +0200

    Updated student history component

--- frontend/projects/main-app/src/app/dashboard/student/student-history/student-history.component.ts
+++ frontend/projects/main-app/src/app/dashboard/student/student-history/student-history.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { StudentSubject } from "../../../core/models/StudentSubject";
+import { StudentService } from "../../../services/student.service";
 
 @Component({
-  selector: 'app-student-history',
-  templateUrl: './student-history.component.html',
-  styleUrls: ['./student-history.component.scss']
+    selector: "app-student-history",
+    templateUrl: "./student-history.component.html",
+    styleUrls: ["./student-history.component.scss"],
 })
 export class StudentHistoryComponent implements OnInit {
+    constructor(private studentService: StudentService) {}
 
-  constructor() { }
+    passedSubjects: StudentSubject[] = [];
 
-  ngOnInit(): void {
-  }
+    notPassedSubjects: StudentSubject[] = [];
 
+    ngOnInit(): void {
+        this.getAllNotPassedSubjects();
+        this.getAllPassedSubjects();
+    }
+
+    getAllPassedSubjects(): void {
+        this.studentService.getAllPassedSubjects().subscribe((res) => {
+            this.passedSubjects = res.payload;
+        });
+    }
+
+    getAllNotPassedSubjects(): void {
+        this.studentService.getAllNotPassedSubjects().subscribe((res) => {
+            this.notPassedSubjects = res.payload;
+        });
+    }
 }
