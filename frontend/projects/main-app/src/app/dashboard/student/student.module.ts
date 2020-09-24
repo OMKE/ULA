082f90e2fdc7d34ec261af2cec81b2e5eab56c47
Omar Iriskic <omaririskic.dev@gmail.com>
2020-09-24 08:52:42 +0200

    Updated student module

--- frontend/projects/main-app/src/app/dashboard/student/student.module.ts
+++ frontend/projects/main-app/src/app/dashboard/student/student.module.ts
@@ -5,12 +5,12 @@ import { SharedModule } from "../../shared/shared.module";
 import { StudentComponent } from "../student/student.component";
 import { StudentChangePasswordComponent } from "./student-change-password/student-change-password.component";
 import { StudentChangeProfileImageComponent } from "./student-change-profile-image/student-change-profile-image.component";
-import { StudentExamsComponent } from "./student-exams/student-exams.component";
 import { StudentHistoryComponent } from "./student-history/student-history.component";
 import { StudentNavbarComponent } from "./student-navbar/student-navbar.component";
 import { StudentNotificationsComponent } from "./student-notifications/student-notifications.component";
 import { StudentProfileComponent } from "./student-profile/student-profile.component";
 import { StudentRoutingModule } from "./student-routing.module";
+import { StudentSubjectComponent } from "./student-subject/student-subject.component";
 import { StudentSubjectsComponent } from "./student-subjects/student-subjects.component";
 import { StudentUpdateProfileComponent } from "./student-update-profile/student-update-profile.component";
 
@@ -25,7 +25,7 @@ import { StudentUpdateProfileComponent } from "./student-update-profile/student-
         StudentSubjectsComponent,
         StudentNotificationsComponent,
         StudentHistoryComponent,
-        StudentExamsComponent,
+        StudentSubjectComponent,
     ],
     imports: [CommonModule, StudentRoutingModule, FormsModule, SharedModule],
 })
