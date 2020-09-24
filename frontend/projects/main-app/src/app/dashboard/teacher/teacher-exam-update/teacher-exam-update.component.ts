3ede1f3d707b639be7dfb7233563e3486b80186f
Lazar Filipovic <laki098@gmail.com>
2020-09-24 08:51:11 +0200

    Added teacher exam update component

---
+++ frontend/projects/main-app/src/app/dashboard/teacher/teacher-exam-update/teacher-exam-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-exam-update',
+  templateUrl: './teacher-exam-update.component.html',
+  styleUrls: ['./teacher-exam-update.component.scss']
+})
+export class TeacherExamUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
