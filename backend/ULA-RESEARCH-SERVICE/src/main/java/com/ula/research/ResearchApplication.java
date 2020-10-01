32f11ccc57a30281d9a79c0da6d1204b718fe42a
Omar Iriskic <omaririskic.dev@gmail.com>
2020-10-01 18:24:42 +0200

    Added Research Service

---
+++ backend/ULA-RESEARCH-SERVICE/src/main/java/com/ula/research/ResearchApplication.java
@@ -0,0 +1,15 @@
+package com.ula.research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ResearchApplication.class, args);
+    }
+
+}
---
+++ backend/ULA-RESEARCH-SERVICE/src/test/java/com/ula/research/ResearchApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ula.research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
