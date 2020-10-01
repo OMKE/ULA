632405b7771249e1e102240b704dd16f1855a7dc
Omar Iriskic <omaririskic.dev@gmail.com>
2020-10-01 18:25:57 +0200

    Added admin nvabar and updated admin component

--- frontend/projects/main-app/src/app/dashboard/admin/admin.module.ts
+++ frontend/projects/main-app/src/app/dashboard/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AdminComponent } from "../admin/admin.component";
 import { AdminRoutingModule } from "./admin-routing.module";
+import { AdminNavbarComponent } from './admin-navbar/admin-navbar.component';
 
 @NgModule({
-    declarations: [AdminComponent],
+    declarations: [AdminComponent, AdminNavbarComponent],
     imports: [CommonModule, AdminRoutingModule],
 })
 export class AdminModule {}
