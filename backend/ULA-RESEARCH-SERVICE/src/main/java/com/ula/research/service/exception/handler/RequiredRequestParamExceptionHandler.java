4ac0fab9e63f9da91dcd611dc93c28d71adf9494
Omar Iriskic <omaririskic.dev@gmail.com>
2020-10-12 04:12:45 +0200

    Added exceptions and handlers

---
+++ backend/ULA-RESEARCH-SERVICE/src/main/java/com/ula/research/service/exception/handler/RequiredRequestParamExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.ula.research.service.exception.handler;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.ula.core.exception.handler.RequiredRequestParameterExceptionHandler;
+
+@RestControllerAdvice
+public class RequiredRequestParamExceptionHandler extends RequiredRequestParameterExceptionHandler
+{
+}
